697d0aa2460491102fd232bfcb5fdb0ed152620a
nico1988 <20182726@csuft.edu.cn>
2025-04-21 09:11:23 +0800

    feat: update defer

--- 7-defer/defer.go
+++ 7-defer/defer.go
@@ -2,12 +2,30 @@ package main
 
 import "fmt"
 
+func returnFunc() int {
+	deferFunc()
+	fmt.Println("return end")
+	return 1
+}
+
+func deferFunc() {
+	fmt.Println("defer end1")
+	fmt.Println("defer end2")
+
+}
+
+func init() {
+	//defer关键字
+	fmt.Printf("init::hello go 1\n")
+}
+
 func main() {
 	//写入defer关键字
 	defer fmt.Println("main end1")
 	defer fmt.Println("main end2")
 
-
 	fmt.Println("main::hello go 1")
 	fmt.Println("main::hello go 2")
+
+	returnFunc()
 }
