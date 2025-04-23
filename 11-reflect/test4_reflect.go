ce4df609f1b6d62001728d94ad31c833d8327d68
nico1988 <20182726@csuft.edu.cn>
2025-04-23 22:42:38 +0800

    feat: update reflect

--- 11-reflect/test4_reflect.go
+++ 11-reflect/test4_reflect.go
@@ -10,8 +10,15 @@ func reflectNum(arg interface{}) {
 	fmt.Println("value : ", reflect.ValueOf(arg))
 }
 
+func reflectStr(arg interface{}) {
+	fmt.Println("type : ", reflect.TypeOf(arg))
+	fmt.Println("value : ", reflect.ValueOf(arg))
+}
+
 func main() {
 	var num float64 = 1.2345
+	var str string = "hello world"
 
 	reflectNum(num)
+	reflectStr(str)
 }
